565ba7421000bf42191b4526acffb8fa32b14de8
Your Name <you@example.com>
2024-04-12 18:22:56 +0100

    creation_de_class_conge

---
+++ readyToWorkGRH/src/tn/esprit/entity/Congé.java
@@ -0,0 +1,19 @@
+public class Congé {
+   
+    private int idC;
+    private String description;
+    private boolean statut;
+ 
+ 
+ 
+    public  Congé(int idC, String description, boolean statut) {
+        this.idC = idC;          
+        this.description = description;
+        this.statut = statut;
+}
+ 
+public void ajouteCongé(idC, description, statut){
+   
+}
+ 
+}
